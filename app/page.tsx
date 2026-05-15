e7c196d7acbcbb35ed6b07d87181ad0c4b48c4f7
Nicholas <nskins@umich.edu>
2026-05-15 18:33:56 -0400

    Update introduction page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div className="max-w-2xl">
       <div className="text-2xl font-bold px-16 pt-4 pb-2">Hey, I&apos;m Nicholas!</div>
-      <div className="text-xl px-16 py-2">I&apos;m a Staff Software Engineer with 8+ years of experience building scalable, reliable, and maintainable software. I am currently employed at <PageLink href="https://vehlo.com">Vehlo</PageLink> and working on best-in-class solutions for the automotive care industry. My primary focus is on technical leadership and user-centered full-stack web development.</div>
-      <div className="text-xl px-16 py-2">I&apos;m passionate about building products that users love! In my free time I enjoy running, playing piano, cooking, reading books, and learning foreign languages.</div>
+      <div className="text-xl px-16 py-2">I'm a Staff Software Engineer with 8+ years of experience designing and implementing secure, reliable software products. I have served both as a technical leader and as an individual contributor; my ideal position consists of coding, mentoring engineers, and establishing a sensible technical strategy.</div>
+      <div className="text-xl px-16 py-2">In my current professional capacity, I use C# and TypeScript for full-stack application development; however, I also have extensive experience in other technologies. In the open source community, I have contributed to <PageLink href="https://github.com/tokio-rs/tokio/pulls?q=is%3Apr+author%3Anskins">Tokio, an asynchronous runtime in Rust</PageLink>; <PageLink href="https://github.com/elixir-nx/axon/pulls?q=is%3Apr+author%3Anskins">Axon, a neural network library in Elixir</PageLink>; and <PageLink href="https://github.com/cpeikert/Lol/pull/19">Λ⚬λ, a cryptography library in Haskell</PageLink>. I love learning and am confident in my ability to pick up new technologies as needed.</div>
+      <div className="text-xl px-16 py-2">I love running! I am currently working towards my goal of completing a marathon in under 3 hours. I also enjoy playing piano, cooking, reading books, and learning foreign languages. I recently passed the Japanese-Language Proficiency Test (JLPT) at the N4 level.</div>
     </div>
   );
 }
